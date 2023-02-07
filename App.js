6f996953fd7224d07f38ea5af3a0b1bb3dbe44e9
Navya Polishetti <navya.polishetti@gmail.com>
2023-02-07 10:37:51 +0100

    Lecture-04: using static data

--- App.js
+++ App.js
@@ -1,52 +1,80 @@
-/**
- * Parcel offers so many special powers to optimize our App.
- *
- * HMR(Hot module replacement)
- * File watching Alogorithm(it is written in c++)
- * Bundling
- * Minifing the code
- * Dev and production build
- * Super fast build Algorithm
- * Image Optimization
- * caching while development
- * compression
- * Compatiable with older version browsers
- * https on dev(npm parcel index.html --https)
- * port number
- * consistent hashing algorithm
- * zero config
- *
- *
- *
- */
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-import React from "react";
-import ReactDOM from "react-dom/client";
+const Title = () => {
+  return (
+    <a href="/">
+      <img
+        className="logo"
+        alt="food-logo"
+        src="https://yt3.googleusercontent.com/ytc/AL5GRJXudT76175T4x4n7eslWM1YkgNLHDSSqfXGoadl=s900-c-k-c0x00ffffff-no-rj"
+      />
+    </a>
+  )
+}
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Heading1"
-);
+const KFC = {
+  name: "KFC",
+  image: "",
+  cuisines: ["American", "Burgers"],
+  travel: 10,
+}
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title2",
-  },
-  "Heading2"
-);
+const RestroCard = () => {
+  return (
+    <div className="restro-card">
+      <img
+        alt="food-image"
+        src="https://www.bighospitality.co.uk/var/wrbm_gb_hospitality/storage/images/_aliases/wrbm_large/publications/hospitality/bighospitality.co.uk/article/2018/03/01/a-week-after-running-out-of-chicken-kfc-runs-out-of-gravy/2759203-1-eng-GB/A-week-after-running-out-of-chicken-KFC-runs-out-of-gravy.png"
+      ></img>
+      <h2>{KFC.name}</h2>
+      <h3>{KFC.cuisines.join(", ")}</h3>
+      <h4>{KFC.travel} mins</h4>
+    </div>
+  )
+}
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading2]
-);
+const Body = () => {
+  return (
+    <div className="restro-list">
+      <RestroCard />
+      <RestroCard />
+      <RestroCard />
+      <RestroCard />
+      <RestroCard />
+    </div>
+  )
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const Footer = () => {
+  return <h4>Footer</h4>
+}
 
-root.render(container);
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<AppLayout />)
