4eaa47260bbde50c115600e3ad4a163672af0d0b
Navya Polishetti <navya.polishetti@gmail.com>
2023-02-08 09:09:43 +0100

    Lecture_02-Igniting our App

---
+++ Lecture_02/App.js
@@ -0,0 +1,55 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+/***
++ *
+* Parcel SUPER POWERS
+
++ * Created A Server
++ * HMR - Hot Module Replacement
++ * File Watcher algorithm - C++
++ * BUNDLING
++ * MINIFY
++ * Cleaning our Code
++ * Dev abd Production Build
++ * Super Fast build algorithm
++ * Image Optimization
++ * Caching while development
++ * Compression
++ * Compatble with older version of browser
++ * HTTPS on dev
++ * port Number
++ * Consistent Hashing Algorithm
++ * Zero Config
++ * Tree shaking
++ *
++ *
++ * Transitive Dependencies
++ */
+
+const h2 = React.createElement(
+  "h2",
+  {
+    title: "heading", //attributes
+  },
+  "Namasthe React!!!"
+)
+
+const h3 = React.createElement(
+  "h3",
+  {
+    title: "heading2",
+  },
+  "Namasthe Everyone"
+)
+
+const container = React.createElement(
+  "container",
+  {
+    id: "container",
+  },
+  [h2, h3]
+)
+
+console.log(h2)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(container)
