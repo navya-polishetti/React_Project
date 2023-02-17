62014e6917d69eb441382a5e51a8861cc2fd2d0f
Navya Polishetti <navya.polishetti@gmail.com>
2023-02-17 16:26:41 +0100

    React_project

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,23 +1,54 @@
 import { restroList } from "./constants"
 import RestroCard from "./RestroCard"
+import { useState } from "react"
+
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.data?.name?.includes(searchText)
+  )
+  console.log(searchText)
+  return filterData
+}
 
 const Body = () => {
-  return (
-    <div className="restro-list">
-      {restroList.map((restro) => {
-        return <RestroCard {...restro.data.data} key={restro.data.data.id} />
-      })}
+  //const searchTxt = "Burger King"
+  //searchText variable is a local state variable
+  //setSearchText(it is like method to update the respective local variable) is set the local state variable -- binding the local state variable with UI
+  //To keep track of the variables or you need to in sync with UI that's why we need to use the state variables
+  //above if you change the something in UI you it will UI will be renders the automatically
+  const [searchText, setSearchText] = useState("")
+  const [restaurants, setRestaurants] = useState(restroList)
 
-      {/*<RestroCard {...restroList[0].data.data} />
-        <RestroCard {...restroList[1].data.data} />
-        <RestroCard {...restroList[2].data.data} />
-        <RestroCard {...restroList[3].data.data} />
-        <RestroCard {...restroList[4].data.data} />
-        <RestroCard {...restroList[5].data.data} />
-        <RestroCard {...restroList[6].data.data} />
-        <RestroCard {...restroList[7].data.data} />
-        <RestroCard {...restroList[8].data.data} /> */}
-    </div>
+  return (
+    <>
+      <div className="Search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search for a restro"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        ></input>
+
+        <button
+          className="search-btn"
+          onClick={() => {
+            //need to filter the data
+            const data = filterData(searchText, restaurants)
+            //Update the state of reataurants with filterd data
+            setRestaurants(data)
+          }}
+        >
+          Search
+        </button>
+      </div>
+
+      <div className="restro-list">
+        {restaurants.map((restro) => {
+          return <RestroCard {...restro.data.data} key={restro.data.data.id} />
+        })}
+      </div>
+    </>
   )
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,6 +14,7 @@ const Header = () => {
   return (
     <div className="header">
       <Title />
+
       <div className="nav-items">
         <ul>
           <li>Home</li>
