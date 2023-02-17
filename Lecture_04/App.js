d388a75efc0e56ece3c18cb021584f8ca48fdbb8
Navya Polishetti <navya.polishetti@gmail.com>
2023-02-17 12:18:09 +0100

    image URL'S updated

--- Lecture_04/App.js
+++ Lecture_04/App.js
@@ -28,23 +28,28 @@ const Header = () => {
   )
 }
 
-const KFC = {
-  name: "KFC",
-  image: "",
-  cuisines: ["American", "Burgers"],
-  travel: 10,
-}
 
-const RestroCard = () => {
+
+ const KFC = {
+  name: "KFC",
+   image: "https://www.bighospitality.co.uk/var/wrbm_gb_hospitality/storage/images/_aliases/wrbm_large/publications/hospitality/bighospitality.co.uk/article/2018/03/01/a-week-after-running-out-of-chicken-kfc-runs-out-of-gravy/2759203-1-eng-GB/A-week-after-running-out-of-chicken-KFC-runs-out-of-gravy.png",
+   cuisines: ["American", "Burgers"],
+   travel: 10,
+ }
+
+console.log(restroList)
+
+const RestroCard = () => { (props)
+  console.log(props)
   return (
     <div className="restro-card">
       <img
         alt="food-image"
-        src="https://www.bighospitality.co.uk/var/wrbm_gb_hospitality/storage/images/_aliases/wrbm_large/publications/hospitality/bighospitality.co.uk/article/2018/03/01/a-week-after-running-out-of-chicken-kfc-runs-out-of-gravy/2759203-1-eng-GB/A-week-after-running-out-of-chicken-KFC-runs-out-of-gravy.png"
+        src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill" + restroList[0].data?.data.cloudinaryImageID}
       ></img>
-      <h2>{KFC.name}</h2>
-      <h3>{KFC.cuisines.join(", ")}</h3>
-      <h4>{KFC.travel} mins</h4>
+      <h2>{restroList[0].data?.name}</h2>
+      <h3>{restroList[0].data.cuisines.join(", ")}</h3>
+      <h4>{restroList[0].data.lastMileTravel} mins</h4>
     </div>
   )
 }
@@ -52,7 +57,7 @@ const RestroCard = () => {
 const Body = () => {
   return (
     <div className="restro-list">
-      <RestroCard />
+      <RestroCard restro={...restroList}/>
       <RestroCard />
       <RestroCard />
       <RestroCard />
@@ -62,7 +67,7 @@ const Body = () => {
 }
 
 const Footer = () => {
-  return <h4>Footer</h4>
+  return <h4>Footerrrrrrrrrrrr</h4>
 }
 
 const AppLayout = () => {
